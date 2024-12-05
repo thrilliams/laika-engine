7a1d094a2526cf1891ed3819c7b1ae323b58b931
june <juniper@aethre.co>
2024-12-05 14:02:37 -0500

    export logger type for recursively

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export type {
 	MaybeDraft,
 } from "./gameLogic/createHelperWrappers";
 export type { GameType as Game } from "./gameType/GameType";
-export type { ReducerReturnType } from "./helperTypes/Reducers";
+export type { ReducerReturnType, Logger } from "./helperTypes/Reducers";
 
 // also re-export types from immer
 export type { Draft, Immutable } from "immer";
